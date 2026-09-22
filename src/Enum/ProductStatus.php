103ab2393a71a25c3c70c6f5b52e0b90304fe609
Guillaume Gorvel <ggorvel@gmail.com>
2026-09-22 16:15:26 +0200

    feat: add status reserved

--- src/Enum/ProductStatus.php
+++ src/Enum/ProductStatus.php
@@ -5,6 +5,7 @@ namespace App\Enum;
 enum ProductStatus: string
 {
     case AVAILABLE = 'available';
+    case RESERVED = 'reserved';
     case BUYING = 'buying';
     case FUNDED = 'funded';
     case PURCHASED = 'purchased';
@@ -12,6 +13,7 @@ enum ProductStatus: string
     public function toFrench(): string{
         return match ($this){
             self::AVAILABLE => 'disponible',
+            self::RESERVED => 'reservé',
             self::BUYING => 'en cours d\'achat',
             self::FUNDED => 'financé',
             self::PURCHASED => 'acheté',
